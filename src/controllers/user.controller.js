50ad21e566204a1c54a21e4f7cbe2bd8188d23df
Subham15112001 <sinchan889377888@gmail.com>
2024-07-17 22:45:57 +0530

    change register controller added refreshtoken after creating user and send accessToken to front-end

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,16 +11,16 @@ const generateAccessTokenandRefreshToken = async (userId) => {
         const accessToken = await prisma.user.generateAccessToken(userId);
         const refreshToken = await prisma.user.generateRefreshToken(userId);
 
-        const response =  await prisma.user.update({
-            where : {
-                id : userId
+        const response = await prisma.user.update({
+            where: {
+                id: userId
             },
-            data : {
-                "refreshToken" : refreshToken
+            data: {
+                "refreshToken": refreshToken
             }
         })
         //console.log(response)
-        
+
         return { accessToken, refreshToken };
 
     } catch (error) {
@@ -28,75 +28,94 @@ const generateAccessTokenandRefreshToken = async (userId) => {
     }
 }
 
-const registerUser = asyncHandler(async (req,res,next) => {
-    const {  username, email, password } = req.body;
+const registerUser = asyncHandler(async (req, res, next) => {
+    const { username, email, password } = req.body;
 
-  
-    if ([ username, email].some((value) => value.trim() === "")) {
+
+    if ([username, email].some((value) => value.trim() === "")) {
         throw new ApiError(400, "all field are neccessary")
     }
 
     const existedUser = await prisma.user.findUnique({
-        where : {
-            "email" : email
+        where: {
+            "email": email
         }
     })
 
-    if(existedUser){
-        throw new ApiError(409,"user already exist")
+    if (existedUser) {
+        throw new ApiError(409, "user already exist")
     }
 
     const createUser = await prisma.user.create({
-        data : {
-            "email":email,
-            "password":password,
-            "username":username
+        data: {
+            "email": email,
+            "password": password,
+            "username": username
         }
     })
-  
 
-    if(!createUser){
-        throw new ApiError(500,"something went wrong when creating user")
+    const { refreshToken, accessToken } = await generateAccessTokenandRefreshToken(createUser.id)
+
+    const option = {
+        htmlOnly: true,
+        secure: true
+    }
+
+    const updatedUser = await prisma.user.findUnique({
+        where : {
+            id : createUser.id
+        }
+    })
+
+
+    if (!createUser) {
+        throw new ApiError(500, "something went wrong when creating user")
     }
 
     return res.status(201)
-              .json(new ApiResponse(201,createUser,"created user successfully"))
+        .cookie("refreshToken", refreshToken, option)
+        .cookie("accessToken", accessToken, option)
+        .json(new ApiResponse(201, {
+            user: updatedUser,
+            accessToken,
+            refreshToken
+        }, "created user successfully"))
 })
 
-const loginUser = asyncHandler(async (req,res,next) => {
-    const {email,password} = req.body;
-    
+const loginUser = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
     let userExist = await prisma.user.findUnique({
-        where : {
-            email : email
+        where: {
+            email: email
         }
     })
-    
-    if(!userExist){
-        throw new ApiError(404,"user does not exist")
+
+    if (!userExist) {
+        throw new ApiError(404, "user does not exist")
     }
 
-    let comparePassword = await prisma.user.isPasswordCorrect(userExist.password,password)
+    let comparePassword = await prisma.user.isPasswordCorrect(userExist.password, password)
 
-    if(!comparePassword){
-        throw new ApiError(401,"user password is wrong")
+    if (!comparePassword) {
+        throw new ApiError(401, "user password is wrong")
     }
 
-    const {accessToken,refreshToken} = await generateAccessTokenandRefreshToken(userExist.id,password)
+    const { accessToken, refreshToken } = await generateAccessTokenandRefreshToken(userExist.id, password)
 
     const loginUser = await prisma.user.update({
-        where : {
-            id : userExist.id
+        where: {
+            id: userExist.id
         },
-        data : {
-            refreshToken : refreshToken
+        data: {
+            refreshToken: refreshToken
         }
     })
 
-   // console.log(loginUser)
+    // console.log(loginUser)
 
-    if(!loginUser ){
-        throw new ApiError(405,"error in login")
+    if (!loginUser) {
+        throw new ApiError(405, "error in login")
     }
 
     const option = {
@@ -114,19 +133,19 @@ const loginUser = asyncHandler(async (req,res,next) => {
         },
             "user login successfully"
         ))
-             
+
 })
 
-const logoutUser = asyncHandler(async (req,res,next) => {
+const logoutUser = asyncHandler(async (req, res, next) => {
 
     const user = req?.user;
- 
+
     const response = await prisma.user.update({
-        where : {
-            id : user.id
+        where: {
+            id: user.id
         },
-        data : {
-            refreshToken : null
+        data: {
+            refreshToken: null
         }
     })
 
@@ -142,30 +161,30 @@ const logoutUser = asyncHandler(async (req,res,next) => {
 
 })
 
-const refreshAccessToken = asyncHandler(async (req,res,next) => {
+const refreshAccessToken = asyncHandler(async (req, res, next) => {
 
     const incomingRefreshToken = req?.cookies?.refreshToken || req.body?.refreshToken;
 
-    if(!incomingRefreshToken){
-        throw new ApiError(403,"Unauthorised request")
+    if (!incomingRefreshToken) {
+        throw new ApiError(403, "Unauthorised request")
     }
 
     try {
         const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
 
         const user = await prisma.user.findUnique({
-            where : {
-                id : decodedToken.id
+            where: {
+                id: decodedToken.id
             }
         })
-       // console.log(user)
+        // console.log(user)
 
-        if(!user){
-            throw new ApiError(403,"invalid authorisation")
+        if (!user) {
+            throw new ApiError(403, "invalid authorisation")
         }
 
-        if(incomingRefreshToken !== user.refreshToken){
-            throw new ApiError(403,"refresh token is expire")
+        if (incomingRefreshToken !== user.refreshToken) {
+            throw new ApiError(403, "refresh token is expire")
         }
 
         const { refreshToken, accessToken } = await generateAccessTokenandRefreshToken(user?.id);
@@ -178,7 +197,7 @@ const refreshAccessToken = asyncHandler(async (req,res,next) => {
         return res.status(200)
             .cookie("refreshToken", refreshToken, option)
             .cookie("accessToken", accessToken, option)
-            .json(new ApiResponse(200, { accessToken, refreshToken , user }, "accessToken and refreshToken send successfully"))
+            .json(new ApiResponse(200, { accessToken, refreshToken, user }, "accessToken and refreshToken send successfully"))
     } catch (error) {
         if (error.name === 'TokenExpiredError') {
             throw new ApiError(403, "Refresh token has expired");
@@ -195,4 +214,4 @@ const getCurrentUser = asyncHandler(async (req, res, next) => {
         .json(new ApiResponse(200, req.user, "current user fetched"))
 })
 
-export {registerUser,loginUser,logoutUser,refreshAccessToken,getCurrentUser}
+export { registerUser, loginUser, logoutUser, refreshAccessToken, getCurrentUser }
